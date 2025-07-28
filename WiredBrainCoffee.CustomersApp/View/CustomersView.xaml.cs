7abecbd099d2b44636579622dc9ac373fa4b31e8
Travis Harris <tharris@hldi.org>
2025-07-28 18:25:34 -0400

    finally finished 6th session

--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WiredBrainCoffee.CustomersApp.Data;
+using WiredBrainCoffee.CustomersApp.ViewModel;
 
 namespace WiredBrainCoffee.CustomersApp.View
 {
@@ -20,21 +22,40 @@ namespace WiredBrainCoffee.CustomersApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
+        private CustomersViewModel _viewModel;
+
         public CustomersView()
         {
             InitializeComponent();
+            _viewModel = new CustomersViewModel(new CustomerDataProvider());
+            DataContext = _viewModel;
+            Loaded += CustomersView_Loaded;
         }
+
+        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
+        {
+            await _viewModel.LoadAsync();
+        }
+
         private void ButtonMoveNavigation_Click(object sender, RoutedEventArgs e)
         {
             //var column = (int)customerListGrid.GetValue(Grid.ColumnProperty);
             //var newColumn = column == 0 ? 2 : 0;
             //customerListGrid.SetValue(Grid.ColumnProperty, newColumn);
 
-            var column = Grid.GetColumn(customerListGrid);
+            //var column = Grid.GetColumn(customerListGrid);
 
-            var newColumn = column == 0 ? 2 : 0;
+            //var newColumn = column == 0 ? 2 : 0;
 
-            Grid.SetColumn(customerListGrid, newColumn);
+            //Grid.SetColumn(customerListGrid, newColumn);
+
+            _viewModel.MoveNavigation();
+
+        }
+
+        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
+        {
+            _viewModel.Add();
         }
     }
 }
