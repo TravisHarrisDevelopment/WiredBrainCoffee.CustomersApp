98f5a062155a09032d0dec7a4fdb10ce92789c33
Travis Harris <tharris@hldi.org>
2025-08-05 14:19:37 -0400

    finished up section 9

--- WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
+++ WiredBrainCoffee.CustomersApp/View/CustomersView.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using WiredBrainCoffee.CustomersApp.Data;
 using WiredBrainCoffee.CustomersApp.ViewModel;
 
@@ -22,20 +10,9 @@ namespace WiredBrainCoffee.CustomersApp.View
     /// </summary>
     public partial class CustomersView : UserControl
     {
-        private CustomersViewModel _viewModel;
-
         public CustomersView()
         {
             InitializeComponent();
-            _viewModel = new CustomersViewModel(new CustomerDataProvider());
-            DataContext = _viewModel;
-            Loaded += CustomersView_Loaded;
         }
-
-        private async void CustomersView_Loaded(object sender, RoutedEventArgs e)
-        {
-            await _viewModel.LoadAsync();
-        }
-
     }
 }
---
+++ WiredBrainCoffee.CustomersApp/ViewModel/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading.Tasks;
+using WiredBrainCoffee.CustomersApp.Command;
+
+namespace WiredBrainCoffee.CustomersApp.ViewModel
+{
+   public class MainViewModel:ViewModelBase
+    {
+        private ViewModelBase? _selectedViewModel;
+        public CustomersViewModel CustomersViewModel { get; }
+        public ProductsViewModel ProductsViewModel { get; }
+
+        public DelegateCommand  SelectViewModelCommand { get; }
+
+        public ViewModelBase? SelectedViewModel 
+        { 
+            get => _selectedViewModel;
+            set
+            {
+                _selectedViewModel = value;
+                RaisePropertyChanged();
+            }    
+        }
+
+        public MainViewModel(CustomersViewModel customersViewModel,
+            ProductsViewModel productsViewModel)
+        {
+            CustomersViewModel = customersViewModel;
+            ProductsViewModel = productsViewModel;
+            SelectedViewModel = CustomersViewModel;
+            SelectViewModelCommand = new DelegateCommand(SelectViewModel);
+        }
+
+        private async void SelectViewModel(object? param)
+        {
+            SelectedViewModel = param as ViewModelBase;
+            await LoadAsync();
+        }
+
+        public async override Task LoadAsync()
+        {
+            if (SelectedViewModel != null)
+            {
+                await SelectedViewModel.LoadAsync();
+            }
+        }
+    }
+}
